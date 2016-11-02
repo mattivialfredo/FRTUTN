64c84cab98dd1edd8b410d59a33744c1aa8cbe1b
mattivi <mattivi@192.168.1.47>
2016-11-01 23:02:39 -0300

    primera modificacion 3

--- prueba1/index.php
+++ prueba1/index.php
@@ -8,6 +8,7 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>  Prueba 1 </title>//modifico mattivi
+        //  modificacio 21232131d
        
     </head>
     <body>
